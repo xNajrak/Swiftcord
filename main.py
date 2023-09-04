7a095463e20b40d143e16494c0bf9672f58fb79a
xNajrak <115323532+xNajrak@users.noreply.github.com>
2023-09-04 21:24:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ except:
   import colorama
 
 colorama.init()
-print(f"{colorama.Fore.RED}Buy swiftcord here: discord.gg/arkoselabs for only 5$"
+print(f"{colorama.Fore.RED}Buy swiftcord here: discord.gg/arkoselabs for only 5$")
 os.system("start https://discord.gg/arkoselabs")
 input()
