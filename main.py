412a9c3443c57d1357f8530bb347a879aa3909f1
xNajrak <115323532+xNajrak@users.noreply.github.com>
2023-09-04 21:24:03 +0200

    discord.gg/arkoselabs

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+try:
+  import colorama
+except:
+  os.system("pip install colorama")
+  import colorama
+
+colorama.init()
+print(f"{colorama.Fore.RED}Buy swiftcord here: discord.gg/arkoselabs for only 5$"
+os.system("start https://discord.gg/arkoselabs")
+input()
